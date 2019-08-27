9094ce025e48dffd76b871393dcd8be1bd51fd94
michael.taylor <michael.taylor@my.tccd.edu>
2019-08-26 19:18:52 -0500

    Created some functions.

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
     {
         static void Main ( /*string[] args*/ )
         {
+            //TODO: Move this
             string name = "";
 
             //string if = "";
@@ -33,6 +34,71 @@ namespace HelloWorld
             double totalPay = payRate * hours;
 
             string fullName = "Fred" + " " + "Jones";
+
+            DemoArithmetic ();
+            DemoRelational ();
+            DemoLogical ();
+            DemoConditional ();
+        }
+
+        static void DemoArithmetic ()
+        {
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = hours * payRate;
+
+            double taxes = totalPay * 0.33;
+
+            //Combination operators work
+            hours += 8;
+
+            totalPay = hours * payRate;
+            double newTaxes = totalPay * 0.66;
+        }
+
+        static void DemoRelational ()
+        {
+            int grade = 75;
+            int passingGrade = 60;
+
+            bool isGreater = grade > passingGrade;
+            bool isGreaterThanOrEqual = grade >= passingGrade;
+            bool isLessThan = grade < passingGrade;
+            bool isLessThanOrEqual = grade <= passingGrade;
+            bool isEqual = grade == passingGrade;
+            bool isNotEqual = grade != passingGrade;
+        }
+
+        static void DemoLogical ()
+        {
+            bool value1 = true;
+            bool value2 = false;
+
+            //Both left and right must be true
+            bool logicalAnd = value1 && value2;
+
+            //Either left or right must be true
+            bool logicalOr = value1 || value2;
+
+            //Negates the value
+            bool logicalNot = !value1;
+        }
+
+        static void DemoConditional ()
+        {
+            int grade = 75;
+
+            // C ? T : F
+            // Evaluate C, if true then T is the value of the expression
+            //otherwise F is
+            bool isPassing = grade > 60 ? true : false;
+
+            //Equivalent to what an if statement could do if it were an expression
+            if (grade > 60)
+                isPassing = true;
+            else
+                isPassing = false;
         }
     }
 }
